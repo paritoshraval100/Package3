6bc351654000bb5b9e0f6ef0e58b7fc25ca33d84
your.user.name <paritosh.raval@infostretch.com>
2022-02-21 15:20:26 +0530

    test

--- Tests/Package3Tests/PackageInfo.swift
+++ Tests/Package3Tests/PackageInfo.swift
@@ -6,7 +6,5 @@
         func testAddition() {
             XCTAssertEqual(Package3.addition(first: 2, second: 3), 5)
         }
-        func testMultiplication() {
-            XCTAssertEqual(Package3.multiplication(first: 2, second: 3), 6)
-        }
+
     }
